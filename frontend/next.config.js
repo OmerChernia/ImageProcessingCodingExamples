d2237138a65222da1adb85994f411799ff824c5c
omerchernia <omer12899@gmail.com>
2025-01-31 16:21:58 +0200

    Configure Netlify and Next.js for improved deployment and compatibility
    - Updated Netlify configuration to use Netlify Edge and Node.js 16
    - Modified Next.js configuration to disable SWC minification and support custom image loading
    - Downgraded React and Next.js versions for better compatibility
    - Replaced export script with lint script in package.json

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'custom',
+    unoptimized: true
+  },
+  swcMinify: false
 }
 
 module.exports = nextConfig 
